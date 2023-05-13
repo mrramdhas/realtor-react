99961a6979b25e60e26c71f1f5134eb90c1bd02c
Ramdhas <ramdhas.m@nexergroup.com>
2023-05-13 23:55:21 +0200

    Created pages and releted routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+
 function App() {
   return (
     <>
-      <h1 className="text-5xl font-bold bg-green-100">Hai React!</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/forgotpassword" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
     </>
   );
 }
